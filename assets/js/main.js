f5d744bbba0a617f364b73e2e7e1cad60e082d8a
Pop0097 <dhruvrawat09@gmail.com>
2020-05-23 15:27:29 -0400

    placeHolder

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,24 @@ function showReviews(n){
   }
   reviews[reviewNumber-1].style.display = "block";
 }
+
+/*Send question form email:*/
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "Dhruv_R";
+  var template_id = "template_2VPl8QHL";
+
+  myform.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){
+    	alert("Sent!");
+       myform.find("button").text("Send");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    });
+  return false;
+});
